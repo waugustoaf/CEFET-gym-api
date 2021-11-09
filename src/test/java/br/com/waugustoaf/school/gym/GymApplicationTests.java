277b1af0d758e845ce151b704c400937e38c96ac
Augusto Fernandes <waltheraaf@gmail.com>
2021-11-09 17:14:26 -0300

    Initial commit

---
+++ src/test/java/br/com/waugustoaf/school/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.waugustoaf.school.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
